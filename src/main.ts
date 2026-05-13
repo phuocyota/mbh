3088744128f90abbb03d27c69edceb4aaf465ec9
phuocyota <nhphuoca7tqtpy@gmail.com>
2026-05-13 09:32:05 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -44,7 +44,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`✅ Application running on port ${port}`);
   console.log(
     `📚 Swagger documentation available at http://localhost:${port}/api/docs`,
